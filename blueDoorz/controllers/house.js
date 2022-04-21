ab721df601f3e423a97008eceb81e73378670d5a
titamulyana <titamulyana10@gmail.com>
2022-04-21 14:38:47 +0700

    add mvc

---
+++ blueDoorz/controllers/house.js
@@ -0,0 +1,11 @@
+'use strict'
+const { House, Profile, User } = require('../models/index')
+const { Op } = require('sequelize')
+
+class HouseController{
+
+
+
+}
+
+module.exports = HouseController
---
+++ blueDoorz/controllers/user.js
@@ -0,0 +1,10 @@
+'use strict'
+const { House, Profile, User } = require('../models/index')
+const { Op } = require('sequelize')
+
+class UserController{
+
+
+}
+
+module.exports = UserController
