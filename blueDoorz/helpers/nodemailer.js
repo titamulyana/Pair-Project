1f669958c16a90c598f10ee561c28d8cc23e5cb1
titamulyana <titamulyana10@gmail.com>
2022-04-22 00:07:37 +0700

    validation

---
+++ blueDoorz/helpers/nodemailer.js
@@ -0,0 +1,26 @@
+"use strict"
+const nodemailer = require("nodemailer")
+
+function sendEmail(sendTo, name){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'joindonghacktiv8@gmail.com',
+            pass: 'Pohodeu1'
+        }
+    });
+    
+    var mailOptions = {
+        from: 'joindonghacktiv8@gmail.com',
+        to: `${sendTo}`,
+        subject: 'Pembayaran Rent House',
+        text: `halo ${name}, kamu bisa melakukan pembayaran dengan transfer ke nomor rekening 010101010 Bank BCO atas nama BADMAN`
+    };
+    
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) throw err;
+        console.log('Email sent: ' + info.response);
+    });
+}
+
+module.exports = sendEmail;
